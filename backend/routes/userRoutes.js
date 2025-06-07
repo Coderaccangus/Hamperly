c8b7180de3c936a110cd565aca0ed5d36ae1d256
root <root@DESKTOP-L1CLIL1.localdomain>
2025-06-07 16:47:26 +1000

    fixing correct importing of register and login controllers

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const { register, login } = require('../controllers/userController');
-
 const router = express.Router();
+const { register, login } = require('../controllers/userController');
 
 router.post('/register', register);
 router.post('/login', login);
